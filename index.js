85c95f3b8637ba7889dc9d158940dca6af47f6e6
farihamehereen15 <fariha15-12875@diu.edu.bd>
2022-11-09 07:49:23 +0600

    middleware added

--- index.js
+++ index.js
@@ -17,6 +17,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('photography').collection('services');
+
+        app.get('/homeservices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
 
 
 app.get('/', (req, res) => {
